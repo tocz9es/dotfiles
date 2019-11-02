5e8af75316d92fe0220b9d962bfa8264f3eac26d
tocz9ea <tocz9es@gmail.com>
2019-11-03 00:48:30 +0800

    config dock and launchpad

---
+++ adjustment-dock.sh
@@ -0,0 +1,4 @@
+defaults write com.apple.dock springboard-rows -int 7
+defaults write com.apple.dock springboard-columns -int 8
+defaults write com.apple.dock ResetLaunchPad -bool TRUE
+killall Dock
