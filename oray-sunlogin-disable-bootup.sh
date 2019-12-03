9437eaac6a6f1eb402eeff543494d099c7e6e92b
tocz9ea <tocz9es@users.noreply.github.com>
2019-12-03 15:13:43 +0800

    Update oray-sunlogin-disable-bootup.sh

--- oray-sunlogin-disable-bootup.sh
+++ oray-sunlogin-disable-bootup.sh
@@ -1,4 +1,5 @@
 sudo sed -i '' '/<key>Disabled<\/key>/{n;s/<false\/>/<true\/>/;}' /Library/LaunchAgents/com.oray.sunlogin.agent.plist
+sudo sed -i '' '/<key>RunAtLoad<\/key>/{n;s/<true\/>/<false\/>/;}' /Library/LaunchAgents/com.oray.sunlogin.agent.plist
 sudo sed -i '' '/<key>Disabled<\/key>/{n;s/<false\/>/<true\/>/;}' /Library/LaunchAgents/com.oray.sunlogin.startup.plist
 sudo sed -i '' '/<key>Disabled<\/key>/{n;s/<false\/>/<true\/>/;}' /Library/LaunchDaemons/com.oray.sunlogin.helper.plist
 sudo sed -i '' '/<key>Disabled<\/key>/{n;s/<false\/>/<true\/>/;}' /Library/LaunchDaemons/com.oray.sunlogin.plist
