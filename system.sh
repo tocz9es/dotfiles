f282c0e1641595756653f1c116caf48615de6536
romp6032 <tocz9es@users.noreply.github.com>
2025-08-17 05:05:16 +0000

    Update and rename adjustment-dock.sh to system.sh

--- system.sh
+++ system.sh
@@ -1,4 +1,9 @@
+# launch-pad
 defaults write com.apple.dock springboard-rows -int 6
 defaults write com.apple.dock springboard-columns -int 8
 defaults write com.apple.dock ResetLaunchPad -bool TRUE
+
+# three-fingers-drag
+defaults write -g NSWindowShouldDragOnGesture -bool true
+
 killall Dock
